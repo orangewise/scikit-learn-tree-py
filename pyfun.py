028d6ef3bb0d150fd088d1ad161ec469fe8e1330
ronald (MacBookie Pro) <ronald@orangewise.com>
2020-08-31 08:47:20 +0200

    run pyfun.py

--- pyfun.py
+++ pyfun.py
@@ -1,7 +1,7 @@
 from mlflow import pyfunc
 import pandas as pd
 
-run_id = "ab8008b2b2cb433a8508a9a60ef638df"
+run_id = "827a7f50f2664babb73ef7a417267a59"
 pyfunc_uri = f"runs:/{run_id}/model"
 pyfunc_model = pyfunc.load_model(pyfunc_uri)
 print(f"Loading the scikit-learn model ({pyfunc_uri}) = as PyFunc Model")
